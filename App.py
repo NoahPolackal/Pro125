fdf382c3531938b7b8c55919a72d2856d6a39f6d
Noah Polackal <71651076+NoahPolackal@users.noreply.github.com>
2021-11-11 17:49:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route("/predict-digit", methods=["POST"])
 def predict_data():
-  # image = cv2.imdecode(np.fromstring(request.files.get("digit").read(), np.uint8), cv2.IMREAD_UNCHANGED)
   image = request.files.get("digit")
   prediction = get_prediction(image)
   return jsonify({
